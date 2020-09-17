7cbec054d59ba9c0f468af47969de4dc10179f3f
Mack Mason <mackmason16@gmail.com>
2020-09-17 14:14:55 +0000

    everything

--- main.py
+++ main.py
@@ -6,24 +6,24 @@
 # Breakout: 12
 
 def sum_n(n):
-  if (n >= 0):
+  if (n <= 0):
     return 0
   else:
     return n + sum_n(n-1)
 
 def print_n(s, n):
-  if (n == 0):
-    return
+  if (n == 1):
+   print(s)
   else:
     print(s)
     print_n(s, n-1)
 
 def run():
-  sumNumber = float(input("Enter a float: "))
-  sum_n(sumNumber)
+  sumFloatNumber = float(input("Enter an int: "))
+  sumNumber = int(sum_n(sumFloatNumber))
+  print(f"sum is {sumNumber}.")
   printString = input("Enter a string: ")
-  printTimes = float(input("Enter how many times you wish to print: "))
-  print_n(printString,printTimes)
+  print_n(printString,sumFloatNumber)
 
 if __name__ == "__main__":
   run()
